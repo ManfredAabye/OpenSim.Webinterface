05210cd211bfdd2d9faa58616497dc500e7c889c
ManfredAabye <zainhofer@gmx.de>
2025-02-05 13:57:29 +0100

    05022025

--- index.php
+++ index.php
@@ -6,14 +6,19 @@
     <h2>Willkommen auf der <?php echo SITE_NAME; ?> Startseite</h2>
     <p>Nutze die folgenden Links, um die verschiedenen Seiten zu besuchen:</p>
     <ul>
-        <li><a href="welcomesplashpage.php">Welcome Page</a></li>
-        <li><a href="economycashbook.php">Economy Page</a></li>
         <li><a href="aboutinformation.php">About Page</a></li>
-        <li><a href="registeruser.php">Register Page</a></li>
-        <li><a href="help.php">Help Page</a></li>
-        <li><a href="passwordreset.php">Password Page</a></li>
+        <li><a href="avatarpicker.php">Avatar Picker Page</a></li>
+        <li><a href="economycashbook.php">Economy Page</a></li>
         <li><a href="gridstatus.php">GridStatus Page</a></li>
         <li><a href="gridstatusrss.php">GridStatusRSS Page</a></li>
+        <li><a href="guide.php">Destination Guide Page</a></li>
+        <li><a href="help.php">Help Page</a></li>
+        <li><a href="maptile.php">Maptile Page</a></li>
+        <li><a href="partner.php">Partner Page</a></li>
+        <li><a href="passwordreset.php">Password Page</a></li>
+        <li><a href="registeruser.php">Register Page</a></li>
+        <li><a href="searchservice.php">Search Page</a></li>
+        <li><a href="welcomesplashpage.php">Welcome Page</a></li>
     </ul>
 </main>
 
