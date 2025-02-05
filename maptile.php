b5ff1ce9606ffd3c835c4701b8b6ab8e997c3ccf
ManfredAabye <zainhofer@gmx.de>
2025-02-05 13:46:58 +0100

    05022025

---
+++ maptile.php
@@ -0,0 +1,12 @@
+<?php
+$title = "MapTile";
+include 'include/config.php';
+include 'include/header.php';
+?>
+
+<main>
+    <h2><?php echo SITE_NAME; ?> MapTile Overview</h2>
+    <p>All information related to the MapTile can be found here.</p>
+</main>
+
+<?php include 'include/footer.php'; ?>
